00b4dfa7ea696c7673631e00f8bf7cf0f78ae728
tmrzette <tyler_marzette@yahoo.com>
2018-02-05 17:06:23 -0600

    first commit

---
+++ index2.js
@@ -0,0 +1,68 @@
+//1.
+
+/*
+const arr = ['dog', 'cat', 'puppy', 'cockroach'];
+
+makePlural = (arr) => {
+    return arr.map(arr) 
+    console.log()
+}
+
+
+
+
+
+//2. 
+
+class DiceRoll {
+    constructor(min, max) {
+        this.min = min;
+        this.max = max;
+    }
+    get randomNumber() {
+        return this.calcNumber();
+    }
+
+    randomNumber(min, max) {
+        return Math.random() * (this.max - this.min) + this.min;
+    }
+    
+}
+
+const dice = new DiceRoll(1, 6);
+
+console.log(dice.randomNumber);
+
+
+
+
+
+//3. 
+
+const obj = {
+    name: 'yaz',
+    email: 'yaz@yaz.com',
+    friends: [
+        {
+            name: 'jahan',
+            email: 'jahan@jahan.com'
+        },
+        {
+            name: 'rihanna',
+            email: 'rihanna@rihanna.com'
+        }
+    ]
+};
+
+const friend = new
+function addFriends() {
+
+}
+*/
+
+//4.
+
+document.addEventListener('DOMContentLoaded')
+
+
+
